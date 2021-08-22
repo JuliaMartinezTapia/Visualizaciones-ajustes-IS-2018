c944f3512805005047211eb78abb4647c2d615f8
Julia <78017909+JuliaMartinezTapia@users.noreply.github.com>
2021-08-22 20:11:49 +0200

    Update barcharts.py

--- barcharts.py
+++ barcharts.py
@@ -70,7 +70,7 @@ def grafico_aumento(df_ajuste_graf):
                    height=500,
                    template="plotly_white",
                     hover_data=["Aumento"],
-                   color_discrete_sequence=px.colors.qualitative.Turbo_r)
+                   color_discrete_sequence=px.colors.qualitative.Plotly)
     return fig_1
 
 def grafico_disminucion(df_ajuste_graf):
@@ -83,7 +83,7 @@ def grafico_disminucion(df_ajuste_graf):
                 height=500,
                  template="plotly_white",
                  hover_data=["Disminución"],
-                color_discrete_sequence=px.colors.qualitative.Turbo_r)
+                color_discrete_sequence=px.colors.qualitative.Plotly)
 
     return fig_2
 
