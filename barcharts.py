b6a255f5c09d6506af762d216fc140b8643cc283
Julia <78017909+JuliaMartinezTapia@users.noreply.github.com>
2021-10-25 12:23:42 +0200

    Update barcharts.py

--- barcharts.py
+++ barcharts.py
@@ -54,7 +54,7 @@ def tamano(df_size):
                  x='Num. Compañías',
                  y='Tamaño',
                  template="plotly_white",
-                 color_discrete_sequence=px.colors.qualitative.Inferno,
+                 color_discrete_sequence=px.colors.sequential.Inferno,
                  labels={  # replaces default labels by column name
                      "Tamaño": "Tipo de empresa", "Num. Compañías": "Número de empresas"})
 
