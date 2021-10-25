29435452014896424ef0a7e8888bef9074c6410e
Julia <78017909+JuliaMartinezTapia@users.noreply.github.com>
2021-10-25 11:58:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ csv_path_3 = "datos/dataset ajustes IS 2018 CCAA.csv"
 ft.config_page()
 
 st.sidebar.title("Panel de control")
-st.sidebar.write("HOLAAAAAA")
+st.sidebar.write("HOLITAAAAAA")
 
 #Importo los csv con los datos en formato dataframe
 
