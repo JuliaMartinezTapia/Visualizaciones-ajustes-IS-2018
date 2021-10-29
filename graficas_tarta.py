5544a486ec0e5425fa6cb05fc116e6e49d087f6f
Julia <78017909+JuliaMartinezTapia@users.noreply.github.com>
2021-10-29 12:26:39 +0200

    Update graficas_tarta.py

--- graficas_tarta.py
+++ graficas_tarta.py
@@ -25,7 +25,7 @@ def comp(df_ajustes):#la función se puede simplificar pero la dejo así por aho
                  template="plotly_white",
                  labels={'Todos los sectores': 'Importe en miles de euros'},
                  color_discrete_sequence=px.colors.sequential.Blackbody,
-                 width=500,
+                 width=600,
                  height=600)
                 #color_discrete_sequence=px.colors.diverging.Portland)
         
