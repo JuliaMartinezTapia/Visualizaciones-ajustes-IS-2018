4ff283f308d6f628dfdd525a6e8bb29c1daad324
Julia <78017909+JuliaMartinezTapia@users.noreply.github.com>
2021-11-18 22:02:38 +0100

    Update graficas_tarta.py

--- graficas_tarta.py
+++ graficas_tarta.py
@@ -25,8 +25,8 @@ def comp(df_ajustes):#la función se puede simplificar pero la dejo así por aho
                  template="plotly_white",
                  labels={'Todos los sectores': 'Importe en miles de euros'},
                  color_discrete_sequence=px.colors.qualitative.T10,
-                 width=600,
-                 height=600)
+                 width=700,
+                 height=700)
                  
     #comp_aum.update_layout(showlegend=False)
 
@@ -54,8 +54,8 @@ def comp(df_ajustes):#la función se puede simplificar pero la dejo así por aho
                  template="plotly_dark",
                  labels={'Todos los sectores': 'Importe en miles de euros', "Partidas": "Ajuste fiscal"},
                  color_discrete_sequence=px.colors.qualitative.T10,
-                 width=600,
-                 height=600)
+                 width=700,
+                 height=700)
 
     comp_dism.update_traces(textposition='inside', textinfo='percent')
 
